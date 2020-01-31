bbfd5b2e21197a3b6f48f8a408d316d661c14021
Carlos González <cargonamt@gmail.com>
2020-01-31 13:07:59 +0100

    please

--- starter_code/sounds.js
+++ starter_code/sounds.js
@@ -2,7 +2,7 @@
      constructor(ctx) {
          this._ctx = ctx
          this._sound = new Audio();
-         this._sound.src = "./sound/bgMusic.mp3";
+         this._sound.src = "./starter_code/sound/bgMusic.mp3";
          this._sound.setAttribute("preload", "auto");
          this._sound.setAttribute("controls", "none");
          this._sound.style.display = "none";
@@ -11,7 +11,7 @@
      play() {
          this._sound.play()
      }
-     
+
      stop() {
          this._sound.pause()
      }
